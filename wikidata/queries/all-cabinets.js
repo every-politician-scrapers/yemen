cb04a29caa1585c2048c51ddc95759f22d049c0f
Tony Bowden <tony@tmtm.com>
2022-11-27 11:54:54 +0200

    streamline all-cabinets SPARQL

--- wikidata/queries/all-cabinets.js
+++ wikidata/queries/all-cabinets.js
@@ -8,11 +8,8 @@ module.exports = function () {
 
   return `SELECT DISTINCT ?ordinal ?item ?itemLabel
                   ?inception ?inceptionPrecision
-                  ?startTime ?startTimePrecision
                   ?abolished ?abolishedPrecision
-                  ?endTime   ?endTimePrecision
                   ?replaces ?replacesLabel ?replacedBy ?replacedByLabel
-                  ?follows ?followsLabel ?followedBy ?followedByLabel
   WHERE {
     { ?item wdt:P31/wdt:P279* wd:Q640506 ; wdt:P1001 wd:${meta.jurisdiction.id} }
     ${cabinet}
@@ -20,19 +17,13 @@ module.exports = function () {
 
     OPTIONAL { ?item p:P31/pq:P1545 ?ordinal }
     OPTIONAL { ?item p:P571 [ a wikibase:BestRank ; psv:P571 [wikibase:timeValue ?inception ; wikibase:timePrecision ?inceptionPrecision] ] }
-    OPTIONAL { ?item p:P580 [ a wikibase:BestRank ; psv:P580 [wikibase:timeValue ?startTime ; wikibase:timePrecision ?startTimePrecision] ] }
     OPTIONAL { ?item p:P576 [ a wikibase:BestRank ; psv:P576 [wikibase:timeValue ?abolished ; wikibase:timePrecision ?abolishedPrecision] ] }
-    OPTIONAL { ?item p:P582 [ a wikibase:BestRank ; psv:P582 [wikibase:timeValue ?endTime   ; wikibase:timePrecision ?endTimePrecision]   ] }
 
     OPTIONAL { ?item wdt:P1365 ?replaces     }
     OPTIONAL { ?item wdt:P1366 ?replacedBy   }
-    OPTIONAL { ?item wdt:P155 ?follows       }
-    OPTIONAL { ?item wdt:P156 ?followedBy    }
-
-    BIND(COALESCE(?inception, ?startTime) AS ?start)
 
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
   }
   # ${new Date().toISOString()}
-  ORDER BY ?start ?item`
+  ORDER BY ?inception ?item`
 }
